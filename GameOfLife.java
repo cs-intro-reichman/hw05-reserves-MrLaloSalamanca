a611ff9f5c40f82473f3b5e372fc57d7deb86639
Shahaf Lalo <shahaf.lalo@post.runi.ac.il>
2024-03-09 17:40:19 +0200

    seacond Version: 17:40, 09/03/2024

--- GameOfLife.java
+++ GameOfLife.java
@@ -118,7 +118,7 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for (int i = 1; i < arr.length-1; i++) {
 			for (int j = 1; j < arr[i].length-1; j++) {
-				System.out.printf("%d ", arr[i][j]);
+				System.out.printf("%3d ", arr[i][j]);
 			}
 			System.out.println();
 		}
@@ -150,7 +150,7 @@ public class GameOfLife {
 
 
 	public static void show1 (int[][] board) {
-		StdDraw.setCanvasSize(900, 900);
+		StdDraw.setCanvasSize(10000, 10000);
 		int rows = board.length;
 		int cols = board[0].length;
 		StdDraw.setXscale(0, cols);
